4ffcc865290eae640d82d39b5070559c09466180
Klaus Boeger <klausboeger@hotmail.com>
2024-09-26 05:45:51 -0300

    progessing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import Modal from "react-modal";
 import { Dashboard } from "./Components/Dashboard";
 import { Header } from "./Components/Header";
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./Components/NewTransactionModal";
+
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalOpen, setisNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setisNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setisNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
