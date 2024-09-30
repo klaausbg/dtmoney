a21f9320424c9cb7aec12fc3c32ec20556321ab9
klaausbg <128738771+klaausbg@users.noreply.github.com>
2024-09-30 19:15:12 -0300

    hope it works

--- src/Components/NewTransactionModal/index.tsx
+++ src/Components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,7 +12,9 @@ export function NewTransactionModal({
 }: NewTransactionModalProps) {
   return (
     <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Confirmar transacao</h2>
+      <Container>
+        <h2>Confirmar transacao</h2>
+      </Container>
     </Modal>
   );
 }
--- src/Components/NewTransactionModal/styles.ts
+++ src/Components/NewTransactionModal/styles.ts
@@ -1,3 +1,39 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  h2 {
+    color: var(--text-title);
+    font-size: 1.5rem;
+    margin-bottom: 2.5rem;
+  }
+  input {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    border-radius: 0.25rem;
+    border: 1px solid #d7d7d7;
+    background: #e7e7ee;
+
+    font-weight: 400;
+    font-size: 1rem;
+
+    &::placeholder {
+      color: var(--text-bbody);
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+  button[type="submit"] {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    background: var(--green);
+    color: #fff;
+    border-radius: 0.25rem;
+    border: 0;
+    font-size: 1rem;
+    margin-top: 1.5rem;
+  }
+`;
