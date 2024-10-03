fed97c395414ce64d027d087549ab9b24c670f57
Klaus Boeger <klausboeger@hotmail.com>
2024-10-03 06:41:05 -0300

    hey there

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -34,7 +34,7 @@ body{
 };
 
 border-style, :-ms-input-placeholder, textarea, input{
-    font-family: 'Poppings', sans-serif;
+    font-family: 'Poppins', sans-serif;
     font-weight: 400;
 }
 
@@ -56,4 +56,40 @@ button{
 body{
     background: var(--background);
     -webkit-font-smoothing: antialiased
-}`;
+}
+
+
+.react-modal-overlay{
+background: rgba(0, 0, 0, 0.5);
+position: fixed;
+top: 0;
+bottom:0;
+right:0;
+left:0 ;
+display: flex;
+align-items: center;
+justify-content: center;
+}
+
+.react-modal-content{
+width: 100%;
+max-width:576px;
+background: var(--background);
+padding: 3rem;
+position: relative;
+border-radius: 0.24rem;
+}
+
+.react-modal-close{
+position: absolute;
+right: 1.5rem;
+top: 1.5rem;
+border: 0;
+background: transparent;
+transition: filter 0.2s;
+
+
+&:hover{
+filter: brightness(0.8)
+}}
+    `;
